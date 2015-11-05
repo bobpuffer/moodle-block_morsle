0979dbcb24cd98c043ee16859059a7c43fa9e390
Bob Puffer <puffro01@luther.edu>
2015-11-05 12:23:03 -0600

    restored m_calendar functionality

--- m_calendar.php
+++ m_calendar.php
@@ -2,9 +2,6 @@
 require_once realpath(dirname(__FILE__) . '/../../config.php');
 require_once("$CFG->dirroot/repository/morsle/lib.php");
 $morsle = new repository_morsle(1);
-//$status = $morsle->m_calendar();
 $course = strtolower("'60301-ACCTG-250-A'");
-// determine rosters for everything else based on visibility of course, removing students if not visible
-$status = $morsle->m_maintain($course);
-
+$status = $morsle->m_calendar($course);
 ?>
